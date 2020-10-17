d43da0f56997e5aa06dde5ca12d3805c26adaa93
hacker1235 <61475929+hacker1235@users.noreply.github.com>
2020-10-17 19:47:02 +0330

    alireza

---
+++ age/age/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 42;
+	switch (age) {
+	case 16:
+		cout << "Too young" << endl;
+	case 42:
+		cout << "Adult" << endl;
+	case 70:
+		cout << "Senior" << endl;
+	default:
+		cout << "This is the default" << endl;
+	}
+	system("pasue");
+}
