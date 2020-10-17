4259c9176f933b6a7dcbb1c466992f1fc4d28389
hacker1235 <61475929+hacker1235@users.noreply.github.com>
2020-10-17 19:27:03 +0330

    alireza

---
+++ Sum/Sum/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	int sum;
+	cout << "Enter a number \n";
+	cin >> a;
+	cout << "Enter another number \n";
+	cin >> b;
+	sum = a + b;
+	cout << "Sum is: " << sum << endl;
+	system("pause");
+}
