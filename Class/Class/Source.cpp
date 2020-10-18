0e3446c673f7e0687cf646fdc9edc2d996e9d391
hacker1235 <61475929+hacker1235@users.noreply.github.com>
2020-10-18 11:26:04 +0330

    alireza

---
+++ Class/Class/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+public:
+	void sayHi() {
+		cout << "Hi" << endl;
+	}
+};
+
+int main() 
+{
+	BankAccount test;
+	test.sayHi();
+	system("pause");
+}
